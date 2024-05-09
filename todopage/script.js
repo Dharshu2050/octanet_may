a53ca3b4bf5fe48aca4e8e11dedd5e69a12a2abf
Dharshu2050 <dharshinieaswaran52@gmail.com>
2024-05-09 19:22:28 +0530

    javascript

---
+++ todopage/script.js
@@ -0,0 +1,10 @@
+(function() {
+    const form=document.getElementById("form")
+    const todo=document.getElementById("todo")
+    const listGroup=document.getElementById("list-group")
+
+    form.addEventListener('add',event >> {
+      event.preventDefault()
+    }) 
+})
+()
